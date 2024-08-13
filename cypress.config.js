04664e6b23e3a65f6d1bae8dff2b06b22f6c5e15
Vinícius <vinigperuzzi@hotmail.com>
2024-08-13 06:07:54 -0300

    finally systems tests for the exams list page

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   e2e: {
-    baseUrl: 'http://localhost:3000', // URL do seu app
-    supportFile: false, // Se não quiser usar suporte customizado
+    baseUrl: 'http://app:3000',
+    supportFile: false,
     setupNodeEvents(on, config) {
       // Eventos de configuração adicionais
     },
+    defaultCommandTimeout: 12000
   },
 };
